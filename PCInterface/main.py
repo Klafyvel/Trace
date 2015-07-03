8f3f7e6fba02002d4dda99aa8e216906c43a43c9
Klafyvel <klafyvel@gmail.com>
2015-07-03 22:28:06 +0200

    Modification de l'interface

--- PCInterface/main.py
+++ PCInterface/main.py
@@ -2,13 +2,11 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
-from window import Ui_MainWindow
+from window import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ui = Ui_MainWindow()
-    window = QMainWindow()
-    ui.setupUi(window)
+    window = MainWindow()
 
     window.show()
     app.exec()
