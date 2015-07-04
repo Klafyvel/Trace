98de9a50d1e41fe67dc633e2c42165c3cc064a05
Klafyvel <klafyvel@gmail.com>
2015-07-04 08:28:08 +0200

    Liste des ports série disponibles.

--- PCInterface/window.py
+++ PCInterface/window.py
@@ -1,11 +1,33 @@
+import serial
+
+from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QMainWindow
+
 from user_interface import Ui_MainWindow
+
+from serial_list import serial_ports
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
+        self.file = ""
+        self.serial = serial.Serial()
+
+        self.btn_serial_ports_list.clicked.connect(self.list_serials)
+
     @pyqtSlot()
     def choose_file(self):
-        #todo
+        pass
+    @pyqtSlot()
+    def list_serials(self):
+        l = serial_ports()
+        for i in range(self.serial_ports_list.count()):
+            self.serial_ports_list.removeItem(0)
+        for i in l:
+            self.serial_ports_list.addItem(i)
+    @pyqtSlot()
+    def connect_serial(self):
+        pass
+    
