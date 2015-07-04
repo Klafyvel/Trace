68f5885a4761e0313ff736c3e427f8ed9deb843e
Klafyvel <klafyvel@gmail.com>
2015-07-04 11:17:56 +0200

    Gestion du port série

--- PCInterface/window.py
+++ PCInterface/window.py
@@ -1,7 +1,7 @@
 import serial
 
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import *
 
 from user_interface import Ui_MainWindow
 
@@ -16,6 +16,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.serial = serial.Serial()
 
         self.btn_serial_ports_list.clicked.connect(self.list_serials)
+        self.btn_connect.clicked.connect(self.connect_serial_manager)
 
     @pyqtSlot()
     def choose_file(self):
@@ -28,6 +29,24 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         for i in l:
             self.serial_ports_list.addItem(i)
     @pyqtSlot()
-    def connect_serial(self):
-        pass
+    def connect_serial_manager(self):
+        if self.serial.isOpen():
+            self.serial.close()
+            self.baudrate.setEnabled(True)
+            self.serial_ports_list.setEnabled(True)
+            self.btn_serial_ports_list.setEnabled(True)
+            self.btn_connect.setText("Connecter")
+            QMessageBox.information(self, "Port série", "Le port série a été fermé.")
+            return
+        self.serial.port = self.serial_ports_list.currentText()
+        self.serial.baudrate = int(self.baudrate.currentText())
+        try:
+            self.serial.open()
+            QMessageBox.information(self, "Port série", "Le port {} a été ouvert à {} bauds".format(self.serial.port, str(self.serial.baudrate)))
+            self.baudrate.setEnabled(False)
+            self.serial_ports_list.setEnabled(False)
+            self.btn_serial_ports_list.setEnabled(False)
+            self.btn_connect.setText("Déconnecter")
+        except serial.serialutil.SerialException:
+            QMessageBox.warning(self, "Port série", "Le port série n'a pas pu être ouvert.")
     
